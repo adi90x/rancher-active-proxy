3b84aef66a89f13ed41f9d5924d12d77c7182fca
AdrienM <amaurel90@gmail.com>
2016-12-15 12:26:11 +0000

    Add new file

---
+++ app/functions.sh
@@ -0,0 +1,88 @@
+[[ -z "${VHOST_DIR:-}" ]] && \
+ declare -r VHOST_DIR=/etc/nginx/vhost.d
+[[ -z "${START_HEADER:-}" ]] && \
+ declare -r START_HEADER='## Start of configuration add by letsencrypt container'
+[[ -z "${END_HEADER:-}" ]] && \
+ declare -r END_HEADER='## End of configuration add by letsencrypt container'
+
+add_location_configuration() {
+    local domain="${1:-}"
+    [[ -z "$domain" || ! -f "${VHOST_DIR}/${domain}" ]] && domain=default
+    [[ -f "${VHOST_DIR}/${domain}" && \
+       -n $(sed -n "/$START_HEADER/,/$END_HEADER/p" "${VHOST_DIR}/${domain}") ]] && return 0
+    echo "$START_HEADER" > "${VHOST_DIR}/${domain}".new
+    cat /app/nginx_location.conf >> "${VHOST_DIR}/${domain}".new
+    echo "$END_HEADER" >> "${VHOST_DIR}/${domain}".new
+    [[ -f "${VHOST_DIR}/${domain}" ]] && cat "${VHOST_DIR}/${domain}" >> "${VHOST_DIR}/${domain}".new
+    mv -f "${VHOST_DIR}/${domain}".new "${VHOST_DIR}/${domain}"
+    return 1
+}
+
+remove_all_location_configurations() {
+    local old_shopt_options=$(shopt -p) # Backup shopt options
+    shopt -s nullglob
+    for file in "${VHOST_DIR}"/*; do
+        [[ -n $(sed -n "/$START_HEADER/,/$END_HEADER/p" "$file") ]] && \
+         sed -i "/$START_HEADER/,/$END_HEADER/d" "$file"
+    done
+    eval "$old_shopt_options" # Restore shopt options
+}
+
+## Docker API
+function docker_api {
+    local scheme
+    local curl_opts=(-s)
+    local method=${2:-GET}
+    # data to POST
+    if [[ -n "${3:-}" ]]; then
+        curl_opts+=(-d "$3")
+    fi
+    if [[ -z "$DOCKER_HOST" ]];then
+        echo "Error DOCKER_HOST variable not set" >&2
+        return 1
+    fi
+    if [[ $DOCKER_HOST == unix://* ]]; then
+        curl_opts+=(--unix-socket ${DOCKER_HOST#unix://})
+        scheme='http:'
+    else
+        scheme="http://${DOCKER_HOST#*://}"
+    fi
+    [[ $method = "POST" ]] && curl_opts+=(-H 'Content-Type: application/json')
+    curl "${curl_opts[@]}" -X${method} ${scheme}$1
+}
+
+function docker_exec {
+    local id="${1?missing id}"
+    local cmd="${2?missing command}"
+    local data=$(printf '{ "AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty":false,"Cmd": %s }' "$cmd")
+    exec_id=$(docker_api "/containers/$id/exec" "POST" "$data" | jq -r .Id)
+    if [[ -n "$exec_id" ]]; then
+        docker_api /exec/$exec_id/start "POST" '{"Detach": false, "Tty":false}'
+    fi
+}
+
+function docker_kill {
+    local id="${1?missing id}"
+    local signal="${2?missing signal}"
+    docker_api "/containers/$id/kill?signal=$signal" "POST"
+}
+
+## Nginx
+reload_nginx() {
+    if [[ -n "${NGINX_DOCKER_GEN_CONTAINER:-}" ]]; then
+        # Using docker-gen separate container
+        echo "Reloading nginx proxy (using separate container ${NGINX_DOCKER_GEN_CONTAINER})..."
+        docker_kill "$NGINX_DOCKER_GEN_CONTAINER" SIGHUP
+    else
+        if [[ -n "${NGINX_PROXY_CONTAINER:-}" ]]; then
+            echo "Reloading nginx proxy..."
+            docker_exec "$NGINX_PROXY_CONTAINER" \
+                        '[ "sh", "-c", "/usr/local/bin/docker-gen -only-exposed /app/nginx.tmpl /etc/nginx/conf.d/default.conf; /usr/sbin/nginx -s reload" ]'
+        fi
+    fi
+}
+
+# Convert argument to lowercase (bash 4 only)
+function lc() {
+	echo "${@,,}"
+}
