bc5920052c8efde7652c05763445181bd5e71e19
Sebastian Schneider <mail@sesc.eu>
2017-03-05 07:23:35 +0100

    publish addresses not services

--- app/avahi-update.sh
+++ app/avahi-update.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 
 # Remove blank lines from generated file.
-sed -i '/^\s*$/d' /app/avahi-generated.service
-
-# Split file by xml root tag.
-csplit -z -s -b '%02d.service' -f '/etc/avahi/services/service-' /app/avahi-generated.service '/<?xml version="1.0" standalone="no"?>/' {*}
+cp /app/avahi-generated.hosts /etc/avahi/hosts
 
 # Reload avahi daemon.
 avahi-daemon --reload
